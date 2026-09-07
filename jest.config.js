42a83f699471b61bec8610c15ecb0dc7f58c3e11
vTrox <117455929+7vTr0x7@users.noreply.github.com>
2026-09-08 00:09:33 +0530

    fix: remove missing Jest tests root

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  roots: ["<rootDir>/src"],
   testMatch: ["**/*.test.ts"],
   moduleFileExtensions: ["ts", "js", "json"],
   clearMocks: true
